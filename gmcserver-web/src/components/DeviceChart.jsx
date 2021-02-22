22cc230ed49a8416d6c795e56963b773f92c5413
vinceh121 <contact@vinceh121.me>
2021-02-22 08:23:22 +0100

    Backend - new graph library

--- gmcserver-web/src/components/DeviceChart.jsx
+++ gmcserver-web/src/components/DeviceChart.jsx
@@ -1,87 +1,94 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import "react-vis/dist/style.css";
-import {
-	FlexibleXYPlot,
-	XAxis,
-	YAxis,
-	HorizontalGridLines,
-	VerticalGridLines,
-	LineSeries,
-	Crosshair,
-} from "react-vis";
 import Loader from "./Loader";
+import { ResponsiveLine } from "@nivo/line";
 import { numericRecordFields } from "../GmcTypes";
-
-function recordToPoints(rec) {
-	const pts = [];
-	for (let f in rec) {
-		if (f === "date" || !rec[f] || rec[f] === "NaN") continue;
-		pts.push({ x: new Date(rec.date), y: rec[f], title: f });
-	}
-	return pts;
-}
 
 function DeviceChart(props) {
 	// we need to transform the timeline from 'record' format
-	// to 'data point' format to feed react-vis
+	// to 'data point' format to feed the graph
 	const [timeline, setTimeline] = useState(null);
-	const [crosshairValues, setCrosshairValues] = useState([]);
-
-	const onNearestX = useCallback(
-		(value, { index }) => {
-			setCrosshairValues(recordToPoints(props.timeline[index]));
-		},
-		[props.timeline]
-	);
 
 	useEffect(() => {
 		if (props.timeline) {
 			let tl = {};
+			for (let f of numericRecordFields) {
+				tl[f] = { id: f, data: [] };
+			}
+
 			for (let r of props.timeline) {
 				for (let f of numericRecordFields) {
-					if (!tl[f]) tl[f] = [];
-
-					tl[f].push({ x: new Date(r.date), y: r[f] });
+					tl[f].data.push({
+						x: r.date,
+						y: r[f] !== "NaN" ? r[f] : null,
+					});
 				}
 			}
-			setTimeline(tl);
-			console.log(tl);
+
+			setTimeline(Object.values(tl));
 		}
 	}, [props.timeline]);
 
 	if (timeline) {
 		return (
-			<FlexibleXYPlot
-				onMouseLeave={() => setCrosshairValues([])}
-				animation={false}
-			>
-				<VerticalGridLines />
-				<HorizontalGridLines />
-				{numericRecordFields.map((name, i) => {
-					return (
-						<LineSeries
-							data={timeline[name]}
-							key={i}
-							onNearestX={onNearestX}
-						/>
-					);
-				})}
-				<XAxis
-					tickLabelAngle={-20}
-					tickFormat={(t) => new Date(t).toLocaleString()}
-					height={500}
-				/>
-				<YAxis />
-				<Crosshair
-					values={crosshairValues}
-					itemsFormat={(pts) =>
-						pts.map((v) => {
-							return { title: v.title, value: v.y };
-						})
-					}
-				/>
-			</FlexibleXYPlot>
+			<ResponsiveLine
+				theme={{
+					background: "#141414",
+					textColor: "#ffffff",
+					fontSize: 14,
+					tooltip: { container: { background: "#000000" } },
+					crosshair: {
+						line: { stroke: "#ffffff" },
+					},
+					axis: {
+						domain: {
+							line: {
+								stroke: "#777777",
+								strokeWidth: 1,
+							},
+						},
+						ticks: {
+							line: {
+								stroke: "#777777",
+								strokeWidth: 1,
+							},
+							text: { fontSize: "9px" },
+						},
+					},
+					grid: {
+						line: {
+							stroke: "#dddddd",
+							strokeWidth: 1,
+						},
+					},
+				}}
+				data={timeline}
+				margin={{ top: 5, right: 5, bottom: 60, left: 30 }}
+				animate={true}
+				curve="linear"
+				useMesh={true}
+				enableSlices="x"
+				xScale={{
+					type: "linear",
+					min: "auto",
+					max: "auto",
+				}}
+				axisLeft={{
+					legendOffset: 12,
+				}}
+				axisBottom={{
+					legendOffset: -12,
+					format: (v) => {
+						const d = new Date(v);
+						return (
+							d.toLocaleDateString() +
+							" " +
+							d.toLocaleTimeString()
+						);
+					},
+					tickRotation: -25,
+				}}
+			/>
 		);
 	} else {
 		return <Loader subTitle="Loading timeline..." />;
