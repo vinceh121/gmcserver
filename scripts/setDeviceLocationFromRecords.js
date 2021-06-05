033bc36ab8a381361bb01d9d44c9bdc4eff9503d
vinceh121 <contact@vinceh121.me>
2021-06-05 13:02:49 +0200

    fix(scrips): handle wierd record formats

--- scripts/setDeviceLocationFromRecords.js
+++ scripts/setDeviceLocationFromRecords.js
@@ -6,7 +6,12 @@ while (cur.hasNext()) {
 	let r = db.records.findOne({location: {$exists: true}, deviceId: dev._id});
 	if (r) {
 		let loc = JSON.parse(JSON.stringify(r.location));
-		loc.coordinates.pop();
+		//loc.coordinates.pop();
+		let coords = loc.coordinates;
+		if (coords[0] < -180 || coords[0] > 180 || coords[1] < -90 || coords[1] > 90) {
+			loc.coordinates = coords.reverse();
+			print("should reverse!");
+		}
 		db.devices.updateOne({_id: dev._id}, {$set: {location: loc}});
 	}
 }
