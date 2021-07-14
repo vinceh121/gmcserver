2bb6f0b75549e81f662fd9ea1d1fdb506b0d5032
vinceh121 <contact@vinceh121.me>
2021-07-14 17:49:44 +0200

    fix(server): wrong bounds check

--- gmcserver-server/src/main/java/me/vinceh121/gmcserver/modules/DeviceModule.java
+++ gmcserver-server/src/main/java/me/vinceh121/gmcserver/modules/DeviceModule.java
@@ -43,13 +43,13 @@ public class DeviceModule extends AbstractModule {
 		final User user = ctx.get(AuthHandler.USER_KEY);
 
 		final String name = obj.getString("name");
-		if (name == null || name.length() > 2 && name.length() < 64) {
+		if (name == null || name.length() < 2 && name.length() > 64) {
 			this.error(ctx, 400, "Invalid name");
 			return;
 		}
 
 		final String model = obj.getString("model");
-		if (model == null || name.length() > 2 && name.length() < 64) {
+		if (model == null || name.length() < 2 && name.length() > 64) {
 			this.error(ctx, 400, "Invalid model");
 			return;
 		}
