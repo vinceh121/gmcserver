973d0ebf9b5a14207d5bac64f524afa53402791f
vinceh121 <contact@vinceh121.me>
2021-12-15 14:45:47 +0100

    chore(web): updated everything

--- gmcserver-web/craco.config.js
+++ gmcserver-web/craco.config.js
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const CracoLessPlugin = require("craco-less");
+const CracoLessPlugin = require("craco-less-fix");
 
 const config = {
 	devServer: {
